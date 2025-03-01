c803b5618fcd96254ba53c3130bf0ca155d86674
Vlada Konkova <luittee93@yandex.ru>
2025-03-01 22:47:53 +0300

    8 спринт(Документация) 2 отправка работы

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,10 @@
+// Интерфейс представления главной страницы
+interface IMain {
+  counter: number;
+  catalog: HTMLElement[];
+  locked: boolean;
+}
+
 //определяем тип для категории товара
 type ProductCategory = 'софт-скил' | 'другое' | 'дополнительное' | 'кнопка' | 'хард-скил';
 
@@ -8,23 +15,23 @@ interface IProduct {
   title: string;  // наименование товара
   category: ProductCategory;  // категория продукта
   price: number | null;  // цена товара
-  description?: string;  // описание товара
+  description: string;  // описание товара
 }
 
 //Интерфейс для списка товаров на главной страницы
-interface ProductList {
+interface IProductList {
   productList: IProduct[];  // массив товаров
 }
 
 //Интерфейс для корзины
-interface OrderBasket {
+interface IOrderBasket {
   basketItems: IProduct[]; // массив товаров в корзине
   totalPrice: number; // общая сумма заказа в корзине
 }
 
 //Интерфейс для заказа
-interface OrderForm {
-  basket: OrderBasket;  // выбранные товары(массив товаров)
+interface IOrderForm {
+  basket: IOrderBasket;  // выбранные товары(массив товаров)
   paymentMethod: string;  // метод оплаты
   address: string;  // адрес доставки
   email: string;  // почта пользователя
